00795794e4cd9f4046249652873d7896486316df
JonyStrins <slipknot1852@gmail.com>
2022-09-06 12:55:13 -0500

    Commit initial

---
+++ app/src/main/java/com/example/miroller/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.miroller
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var etq: TextView
+    lateinit var boton: Button
+    lateinit var img: ImageView
+    lateinit var img2: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_roller)
+
+        boton = findViewById(R.id.btnClickHere)
+        etq = findViewById(R.id.lblroller)
+        img = findViewById(R.id.imgRoller)
+        img2 = findViewById(R.id.imgRoller2)
+
+        img.setImageResource(R.drawable.empty_dice)
+        img2.setImageResource(R.drawable.empty_dice)
+        //val dra = resources.getDrawable(R.drawable.dice_3)
+//        img.setImageDrawable(dra)
+
+        // Forma larga de asignar listener
+//        boton.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(applicationContext, "Boton Presionado", Toast.LENGTH_LONG).show()
+//        })
+
+        boton.setOnClickListener{
+            Toast.makeText(applicationContext, "Boton Presionado", Toast.LENGTH_LONG).show()
+
+//            if(etq.text == "Hola"){
+//                etq.text="Adios"
+//            }else{
+//                etq.text="Hola"
+//            }
+
+
+            // Interpolacion de cadenas con linea ejecutable, siempre empieza con ${metodo o variable}
+//            etq.text="Aleatorio: ${diceRoller()}
+
+
+            img.setImageResource(getRandomDiceImage())
+
+            img2.setImageResource(getRandomDiceImage())
+
+            etq.text="Aleatorios:"
+
+
+
+
+        }
+    }
+
+    fun diceRoller(par: Int = 0): Int{
+        // Genera un rango con el ..
+        return (1..6).random()
+    }
+
+    private fun getRandomDiceImage() : Int{
+        val aleatorio = diceRoller()
+
+        val res = when(aleatorio){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        return res
+    }
+}
